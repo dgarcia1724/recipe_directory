59d2c9385a40e1e69ddd39ed4ba4a5dd686a2906
Danny Garcia <dgarcia1724@berkeley.edu>
2023-11-24 00:16:07 -0800

    89. fresh start

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useFetch } from "../../hooks/useFetch";
 
 // styles
 import "./Create.css";
@@ -12,9 +13,19 @@ function Create() {
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
 
+  const { postData, data, error } = useFetch(
+    "http://localhost:3000/recipes",
+    "POST"
+  );
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(title, method, cookingTime, ingredients);
+    postData({
+      title,
+      ingredients,
+      method,
+      cookingTime: cookingTime + " minutes",
+    });
   };
 
   const handleAdd = (e) => {
