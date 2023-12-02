b9c2d1af252efa84989ebc09b5890625cfb00d53
Danny Garcia <dgarcia1724@berkeley.edu>
2023-12-01 21:26:55 -0800

    111. fresh start

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -32,6 +32,12 @@ function Recipe() {
       });
   }, [id]);
 
+  const handleClick = () => {
+    projectFirestore.collection("recipes").doc(id).update({
+      title: "Something completely different",
+    });
+  };
+
   return (
     <div className={`recipe ${mode}`}>
       {error && <p className="error">{error}</p>}
@@ -46,6 +52,7 @@ function Recipe() {
             })}
           </ul>
           <p className="method">{recipe.method}</p>
+          <button onClick={handleClick}>Update me</button>
         </>
       )}
     </div>
